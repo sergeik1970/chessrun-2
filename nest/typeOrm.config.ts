b5e16650f2ef90f602f314a83dbf5e2c79a2ca9a
Sergey Kudashev <sergey.m.kudashev@gmail.com>
2025-09-11 18:37:30 +0300

    Fix typeOrm config

--- nest/typeOrm.config.ts
+++ nest/typeOrm.config.ts
@@ -5,7 +5,7 @@ export default new DataSource({
     host: process.env.NODE_ENV != "prod" ? "127.0.0.1" : "db",
     port: 5432,
     username: "postgres",
-    password: "postgres",
+    password: process.env.DATABASE_PASSWORD || "postgres",
     database: "postgres",
     entities: ["./src/entities/**/*.ts"],
     migrations: ["./migrations/**/*.ts"],
